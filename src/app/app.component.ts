315c6a5aa79185a0d2a91aeb2bf231c3ed2633b2
AdrianCabello <c.adriancabello@gmail.com>
2025-03-31 19:45:56 -0300

    about done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HomeComponent } from './sections/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
-  title = 'adrian-cabello';
+  title = 'Adrian Cabello';
 }
