7389b0d2277157f50284aa0ef720aeaaad2922ae
Tu Nuevo Nombre <adrian.necoh@gmail.com>
2025-04-02 12:20:19 -0300

    add skeleton

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule, Download, Mail, Linkedin, Github, Twitter } from 'lucide-angular';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
-  imports: [LucideAngularModule]
+  imports: [LucideAngularModule, CommonModule]
 })
 export class HomeComponent {
   protected readonly Download = Download;
@@ -13,4 +13,10 @@ export class HomeComponent {
   protected readonly Linkedin = Linkedin;
   protected readonly Github = Github;
   protected readonly Twitter = Twitter;
+  
+  isLoading = true;
+
+  onImageLoad() {
+    this.isLoading = false;
+  }
 }
