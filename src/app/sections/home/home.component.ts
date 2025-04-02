a61f3c6a108dc99c859d6f3e9863382d71c839c9
Tu Nuevo Nombre <adrian.necoh@gmail.com>
2025-04-02 12:00:45 -0300

    fix: buttons

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Download, Mail, Linkedin, Github, Twitter } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
+  imports: [LucideAngularModule]
 })
-export class HomeComponent {}
+export class HomeComponent {
+  protected readonly Download = Download;
+  protected readonly Mail = Mail;
+  protected readonly Linkedin = Linkedin;
+  protected readonly Github = Github;
+  protected readonly Twitter = Twitter;
+}
